da55ed54d0b9c2265ebcd40875e4c384e2a8233e
Lucas Lago <lago@nic.br>
2020-07-17 18:26:26 -0300

    adicionando tentativas
    adicionando 7 tentativas no código com intervalo de 10 minutos

--- deletados.py
+++ deletados.py
@@ -4,15 +4,23 @@ if __name__ == '__main__':
     import verify_deletion
     from random import shuffle
 
-    
+    start_time = time.time()    
     print("iniciando a coleta de ids... ")
-    ids = database.get_ids()
 
-    print("Coletados " + str(len(ids)) + " ids para checagem...")
+    for i in range(7):
+        print("Tentativa " + str(i))
+        ids = database.get_ids()
 
-    print("Checando se tweets foram removidos...")
-    start_time = time.time()
-    verify_deletion.verify(ids)
+        print("Coletados " + str(len(ids)) + " ids para checagem...")
+
+        print("Checando se tweets foram removidos...")
+
+        try:
+            verify_deletion.verify(ids)
+        except Exception as E:
+            print(E)
+            time.sleep(600)
+
     end_time = time.time()
     print("Terminado em... ")
     print(end_time - start_time)
